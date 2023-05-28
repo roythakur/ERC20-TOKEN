11ef212f60b3dde4e5876ed69344b2843c8a7a94
RAHUL <roythakur0028@gmail.com>
2023-05-28 12:18:32 +0530

    Created state variable of ERC20

--- ERC20/ERC20.sol
+++ ERC20/ERC20.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.9;
@@ -13,3 +12,25 @@ interface IERC20 { //interface ERC20
     function transfer(address receiver, uint amount) external;// returns (bool);
 
 }
+
+
+contract MyToken is IERC20  //contract ERC20Impl
+
+{
+
+
+    string public constant name="CromaCampus";
+
+    string public constant symbol="CRC";
+
+    uint8 public constant decimal=8;
+
+    address owner;
+}
+
+ 
+
+ 
+
+
+   
